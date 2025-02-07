df8d950beb3f27c58e2e36f93d1e362eafee05a1
AndersLemme <a.lemme@hotmail.com>
2025-02-07 22:31:38 +0100

    Added function to give random computer choise

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("test");
+
+console.log("Computer chise: ",getComputerChoise());
+
+function getComputerChoise(){
+    let comp_chice =  Math.floor(Math.random()*3);
+    if (comp_chice === 0) {
+        comp_chice = "Rock";
+    }else if( comp_chice === 1){
+        comp_chice = "Paper";
+    }else{
+        comp_chice = "Scissors";
+    }
+    return comp_chice
+}
