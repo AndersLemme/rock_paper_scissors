46828d5ffad3756a51303ae250933868582088ab
AndersLemme <a.lemme@hotmail.com>
2025-02-08 16:27:41 +0100

    Added getHumanChoice function which retrievs rock paper or scissor from a prompt. and calls itself recursively if the input is wrong. Also cleant up spelling error in getComputerChoice

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-console.log("test");
 
+getHumanChoise();
 console.log("Computer chise: ",getComputerChoise());
 
+
 function getComputerChoise(){
-    let comp_chice =  Math.floor(Math.random()*3);
-    if (comp_chice === 0) {
-        comp_chice = "Rock";
-    }else if( comp_chice === 1){
-        comp_chice = "Paper";
+    let comp_choice =  Math.floor(Math.random()*3);
+    if (comp_choice === 0) {
+        comp_choice = "Rock";
+    }else if( comp_choice === 1){
+        comp_choice = "Paper";
     }else{
-        comp_chice = "Scissors";
+        comp_choice = "Scissors";
     }
-    return comp_chice
+    return comp_choice
+}
+
+function getHumanChoise(){
+ let hum_choice = prompt("Please choose rock, paper or scissors!");
+    if(hum_choice == "Rock" || hum_choice == "rock" || hum_choice == "paper" || hum_choice == "Paper" || hum_choice == "scissors" || hum_choice == "Scissors"){
+        console.log("You choose: ", hum_choice);
+    }else{
+        console.log("Your choise was invalid. Please enter rock, paper or scissors!");
+        getHumanChoise();
+    }
+
+ 
 }
