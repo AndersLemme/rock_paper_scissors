5d81d540ce3051e247a2fcb3c66ec0b523226be3
AndersLemme <a.lemme@hotmail.com>
2025-02-10 22:48:40 +0100

    Started on function for play round. And made input case-insensitive.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+let humanScore = 0, computerScore = 0;
+console.log(humanScore);
+console.log(computerScore);
 
-getHumanChoise();
-console.log("Computer chise: ",getComputerChoise());
 
+let hum_choice = getHumanChoise();
+let comp_choice = getComputerChoise();
+console.log("Computer chise: ", comp_choice);
+playRound(hum_choice, comp_choice);
 
 function getComputerChoise(){
     let comp_choice =  Math.floor(Math.random()*3);
@@ -17,12 +22,30 @@ function getComputerChoise(){
 
 function getHumanChoise(){
  let hum_choice = prompt("Please choose rock, paper or scissors!");
-    if(hum_choice == "Rock" || hum_choice == "rock" || hum_choice == "paper" || hum_choice == "Paper" || hum_choice == "scissors" || hum_choice == "Scissors"){
+    if(  hum_choice.toLocaleLowerCase() == "rock" || hum_choice.toLocaleLowerCase() == "paper" || hum_choice.toLocaleLowerCase() == "scissors" ){
         console.log("You choose: ", hum_choice);
     }else{
         console.log("Your choise was invalid. Please enter rock, paper or scissors!");
         getHumanChoise();
     }
-
- 
+    return hum_choice
 }
+function playRound(h, c){
+    /* Vrite draw as one line first*/
+    if(h.toLocaleLowerCase() == "rock"){
+        if(c.toLocaleLowerCase() == "paper"){
+            console.log("You lose! Paper beats Rock!");
+            computerScore ++;
+        }else if(c.toLocaleLowerCase() == "scissors"){
+            console.log("You win! Rock beats Scissors!");
+            humanScore ++;
+        }else{ /*(c.toLocaleLowerCase() == "rock"){*/
+            console.log("Draw! Both chose Rock!");
+        };
+    }
+}
+/*
+    else if(h.toLocaleLowerCase() == "rock" && c.toLocaleLowerCase() == "paper"){
+        console.log("You lose! Paper beats Rock!");
+        computerScore ++;
+*/
